184e3c4f2b720513b8a4e90aaafd72cb00079abd
Alok Kumar Mishra <alok99893@gmail.com>
2023-08-23 15:09:36 +0530

    formated all the code

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,6 @@
 // store.js
-import { createStore } from 'redux';
-import reducer from './reducer';
+import { createStore } from "redux";
+import reducer from "./reducer";
 
 const store = createStore(reducer);
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom"
-import store from './Redux/store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import store from "./Redux/store";
+import { Provider } from "react-redux";
 
-console.log(store.getState())
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-          <BrowserRouter>
-    <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
-
   </React.StrictMode>
 );
 
